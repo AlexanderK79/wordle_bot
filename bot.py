9930c45eb91316502110bc8c2a13acb471ba2128
A.C. Kraijenoord <a.c.kraijenoord@gmail.com>
2022-03-09 15:58:11 +0100

    webhook_url

--- bot.py
+++ bot.py
@@ -60,8 +60,8 @@ def main():
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook(HEROKU_APP_URL + TOKEN)
+                          url_path=TOKEN,
+                          webhook_url=HEROKU_APP_URL + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
