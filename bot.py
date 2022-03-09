e9eaeedebd6c288a315606cc9c2616a3c2a81192
A.C. Kraijenoord <a.c.kraijenoord@gmail.com>
2022-03-09 15:54:06 +0100

    port

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ Author: liuhh02 https://medium.com/@liuhh02
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-PORT = int(os.environ.get('PORT', 5000))
+PORT = int(os.environ.get('TELEGRAM-PORT', 'fill in using Heroku dashboard'))
 TOKEN = os.environ.get('TELEGRAM-TOKEN', 'fill in using Heroku dashboard')
 HEROKU_APP_URL = os.environ.get('HEROKU_APP_URL', 'fill in using Heroku dashboard')
 
